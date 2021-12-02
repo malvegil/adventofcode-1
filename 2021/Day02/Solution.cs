4eb64ed102ae4ba48baa24b65e023b090e3a652d
Chris Esau <malvegil@gmail.com>
2021-12-02 14:45:22 -0600

    part 2 done

--- 2021/Day02/Solution.cs
+++ 2021/Day02/Solution.cs
@@ -13,13 +13,34 @@ class Solution : Solver {
     public object PartOne(string input) {
         var dir = Directions(input);
         var distance = (from d in dir where (d.Item1).StartsWith('f') select d.Item2).Sum();
-        var height = (from d in dir where (d.Item1).StartsWith('u') select -d.Item2).Sum() + (from d in dir where (d.Item1).StartsWith('d') select d.Item2).Sum();
+        var depth = (from d in dir where (d.Item1).StartsWith('u') select -d.Item2).Sum() + (from d in dir where (d.Item1).StartsWith('d') select d.Item2).Sum();
 
-        return distance * height;
+        return distance * depth;
     }
 
     public object PartTwo(string input) {
-        return 0;
+        var dir = Directions(input);
+        var distance = (from d in dir where (d.Item1).StartsWith('f') select d.Item2).Sum();
+        long depth = 0;
+        long aim = 0;
+
+        foreach (var d in dir)
+        {
+            switch(d.Item1)
+            {
+                case "forward":
+                    depth += (d.Item2 * aim);
+                    break;
+                case "up":
+                    aim -= d.Item2;
+                    break;
+                case "down":
+                    aim += d.Item2;
+                    break;
+            }
+        }
+
+        return depth * distance;
     }
 
     List<Tuple<string,int>> Directions(string input) => (from n in input.Split('\n') select new Tuple<string,int>(n.Split(' ')[0],int.Parse(n.Split(' ')[1]))).ToList();
