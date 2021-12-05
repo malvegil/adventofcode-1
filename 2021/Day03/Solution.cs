1ad9a32ec6f958a0bab657c8fffdb6af8995da28
Chris Esau <malvegil@gmail.com>
2021-12-04 20:48:10 -0600

    day 03 part 1 done

--- 2021/Day03/Solution.cs
+++ 2021/Day03/Solution.cs
@@ -11,10 +11,29 @@ namespace AdventOfCode.Y2021.Day03;
 class Solution : Solver {
 
     public object PartOne(string input) {
-        return 0;
+        var diags = Diagnostics(input);
+        var sums = Enumerable.Range(0, diags[0].Length)
+           .Select(
+               i => diags.Select(s => s[i]).Sum()
+           )
+           .ToArray();
+        
+        var gamma = new StringBuilder();
+        var epsilon = new StringBuilder();
+        for (var i = 0; i < diags[0].Length; i++)
+        {
+            gamma.Append(sums[i] > diags.Count() / 2 ? "1" : "0");
+            epsilon.Append(sums[i] < diags.Count() / 2 ? "1" : "0");
+        }
+        
+        return Convert.ToInt32(gamma.ToString(), 2) * Convert.ToInt32(epsilon.ToString(), 2);
     }
 
     public object PartTwo(string input) {
         return 0;
     }
+
+    List<int[]> Diagnostics(string input) => (from n in input.Split('\n') select Array.ConvertAll(n.ToCharArray(), c => (int)Char.GetNumericValue(c))).ToList();
+
+    
 }
